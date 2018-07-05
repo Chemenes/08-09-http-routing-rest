40802bee9dcee215d3dc7879789aa4184fa29442
Christopher <conrad317@gmail.com>
2018-07-05 14:02:32 -0700

    complete

--- model/turkey.js
+++ model/turkey.js
@@ -1,50 +1,33 @@
 'use strict';
 
 const uuid = require('uuid/v4');
-const storage = require('../lib/storage/memory');
+const storage = require('../lib/storage/main.js');
 
 module.exports = class Turkey {
   constructor(config) {
     this._id = uuid();
     this.createdOn = new Date();
     this.flavor = config.flavor;
-    this.style = config.style || '';
+    this.style = config.style;
   }
 
   save() {
-    return storage.save('Turkeys', this);
+    return storage.save('Turkey', this);
   }
 
-  /**
- * The functions below are all "static" methods on this model.
- * Simply put, that means that you can't use them on instances of this model, but
- * rather use them as top level functions.
- * i.e.
- *    This will use the instance method "save" to save the note we just created
- *    let myNote = new Note({ :'Hi',content:'There'});
- *    myNote.save();
- *
- *    To view a single note you would call the method on the constructor istelf:
- *    Note.fetchOne(id)
- *
- * Note that all of the below methods contain calls on our external storage mechanism
- * to perform their operations
- *
- * @returns {*}
- */
   static fetchAll() {
-    // TODO: Bonus to write code to fetch user id's in the storage moduel
+    return storage.fetchAll('Turkey', this)
   }
 
   static findOne(_id) {
-    return storage.get('Turkeys', _id);
+    return storage.get('Turkey', _id);
   }
 
-  static updateOne(_id) {
-    // TODO: Bonus to write code here teo update a user in the storage module by targeting their ID
+  static updateOne(query) {
+     return storage.updateOne('Turkey', query )
   }
 
   static deleteOne(_id) {
-    // TODO: write code here to delete a user in the storage module by targeting their id
+     return storage.deleteOne('Turkey', _id)
   }
 };
--- route/turkey-router.js
+++ route/turkey-router.js
@@ -20,21 +20,57 @@ module.exports = (router) => {
       });
   });
 
-  // /api/v1/note?id=12335
+
   router.get('/api/v1/turkey', (request, response) => {
-    if (!request.url.query.id) {
-      customResponse.sendError(response, 404, 'Your request requires an id');
+    if(!request.url.query.id) {
+      customResponse.sendError(response, 404,'No ID');
       return undefined;
     }
-
     Turkey.findOne(request.url.query.id)
-      .then((turkey) => {
-        customResponse.sendJSON(response, 200, turkey);
-      })
-      .catch((err) => {
-        console.log(err);
-        customResponse.sendError(response, 404, err.message);
-      });
+    .then((turkey) => {
+      customResponse.sendJSON(response, 200, turkey);
+    })
+    .catch((err) => {
+      console.log(err);
+      customResponse.sendError(response, 404,err.message);
+    });
     return undefined;
   });
+
+  router.delete('.api/v1/turkey', (request, response) => {
+    if (!request.url.query.id) {
+      customResponse.sendError(response, 404, 'No ID');
+      return undefined;
+    }
+    Turkey.deleteOne(request.url.query.id)
+    .then((turkey) => {
+      customResponse.sendJson(response, 200, turkey);
+      console.log(`${request.url.query.id} delete`);
+    })
+    .catch((err) => {
+      console.log(err);
+      customResponse.sendError(response, 404, err.message);
+    });
+    return undefined;
+  });
+
+  router.put('api/v1/turkey', (request, response) => {
+    console.log('PUT /api/v1/turkey');
+    if (!request.body.id) {
+      customResponse.sendError(response, 404, 'Missing id');
+      return undefinied;
+    }
+    Turkey.updateOne(request.body)
+    .then((turkey) => {
+      customeResponse.sendJSON(respsone, 200, turkey);
+      console.log(`${request.body.id} update`);
+    })
+    .catch((err) => {
+      console.log(err);
+      customResponse.sendError(response, 404, err.message);
+    });
+    return undefined;
+
+  });
+
 };
